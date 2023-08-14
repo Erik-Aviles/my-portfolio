73dab2bd4109e962f763862178c43c3a3c732acc
Erika Avilés <eriavicor34@gmail.com>
2023-08-14 14:28:23 -0500

    Responsive (About: biografy, image, historicalData)

--- src/pages/about.js
+++ src/pages/about.js
@@ -41,14 +41,14 @@ export default function about() {
         <meta name="description" content="Informacion personal" />
       </Head>
       <main className="flex w-full flex-col items-center justify-center dark:text-light">
-        <Layout>
+        <Layout className="pt-16">
           <AnimatedText
             text="¡La pasión alimenta el propósito!"
-            className="mb-16"
+            className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl"
           />
 
-          <div className="grid w-full grid-cols-8 gap-16">
-            <div className="col-span-3 flex flex-col items-start justify-start ">
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+            <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8">
               <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
                 Biografía
               </h2>
@@ -57,7 +57,7 @@ export default function about() {
               ))}
             </div>
 
-            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light">
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8">
               <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light " />
               <Image
                 src={profileWomen}
@@ -67,18 +67,19 @@ export default function about() {
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               />
             </div>
-            <div className="col-span-2 flex flex-col items-end justify-between">
+
+            <div className="col-span-2 flex flex-col justify-between xl:col-span-8 xl:flex-row md:order-3">
               {historicalData.map((data) => (
                 <div
                   key={data.id}
-                  className="flex flex-col items-end justify-center"
+                  className="flex flex-col items-end justify-center xl:items-center "
                 >
-                  <span className="inline-block text-5xl font-bold">
+                  <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
                     <AnimatedNumber value={data.time} />+
                   </span>
-                  <h3 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">
+                  <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
                     {data.description}
-                  </h3>
+                  </h2>
                 </div>
               ))}
             </div>
